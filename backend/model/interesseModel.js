c115e7ea49f3ce9fb069532bd61be664d9e1302f
Pedro Antonio <eupedroassuncao@gmail.com>
2025-02-21 20:36:25 -0300

    Adicionando model de interesse

---
+++ backend/model/interesseModel.js
@@ -0,0 +1,46 @@
+const db = require('../db');
+
+exports.list = () => {
+    return new Promise((resolve, reject) => {
+        db.query(`SELECT * FROM interesses`, (err, results) => {
+            if (err) return reject(err);
+            resolve(results);
+        });
+    });
+};
+
+exports.listById = (id) => {
+    return new Promise((resolve, reject) => {
+        db.query(`SELECT * FROM interesses WHERE id = ?`, [id], (err, results) => {
+            if (err) return reject(err);
+            resolve(results[0]);
+        });
+    });
+};
+
+exports.post = (interesse) => {
+    return new Promise((resolve, reject) => {
+        db.query(`INSERT INTO interesses (interesse) VALUES (?)`, [interesse], (err, results) => {
+            if (err) return reject(err);
+            resolve(results);
+        });
+    });
+};
+
+exports.put = (interesse, id) => {
+    return new Promise((resolve, reject) => {
+        db.query(`UPDATE interesses SET interesse = ? WHERE id = ?`, [interesse, id], (err, results) => {
+            if (err) return reject(err);
+            resolve(results);
+        });
+    });
+};
+
+exports.delete = (id) => {
+    return new Promise((resolve, reject) => {
+        db.query(`DELETE FROM interesses WHERE id = ?`, [id], (err, results) => {
+            if (err) return reject(err);
+            resolve(results);
+        });
+    });
+};
