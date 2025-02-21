13ef65ca7a5449fb1d7f16899534b885b7ab6099
Pedro Antonio <eupedroassuncao@gmail.com>
2025-02-20 22:27:51 -0300

    Implementando estrututra

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+const cors = require("cors");
+
+
+app.use(cors());
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello from the backend!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
