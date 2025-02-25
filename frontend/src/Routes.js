26183320b3ba5820bf8ebbd2f2d09729cc67719e
iDjhony <djhonyapple@gmail.com>
2025-02-24 21:05:35 -0300

    Correção de nomes de arquivos

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -2,8 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
 // Páginas <----------------->
-import Home from "./pages/home/homes";
-import Idioma from "./pages/idioma/idiomas";
+import Home from "./pages/home/home";
+import Idioma from "./pages/idioma/idioma";
 import Chat from "./pages/chat/chat";
 import Interesse from "./pages/interesse/interesse";
 import Login from "./pages/login/login";
