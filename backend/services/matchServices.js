4bd8b3735d3f16e12ea29844b97ed1b1ce19d1b5
iDjhony <djhonyapple@gmail.com>
2025-03-03 17:04:11 -0300

    criação do serviço que recomenda os usuarios

---
+++ backend/services/matchServices.js
@@ -0,0 +1,35 @@
+const db = require("../db");
+
+async function buscarMatches(userId) {
+    //buscar idiomas nativos e idiomas que o usuário QUER APRENDER!!!
+    const [userData] = await db.query(`
+            SELECT idioma_nativo_id FROM usuarios WHERE id = ?
+        `, [userId]);
+        
+    const [idiomasAprendidos] = await db.query(
+        `SELECT idioma_id FROM usuarios_idiomas WHERE usuario_id = ?`,
+        [userId]
+    );
+
+    if (!userData || idiomasAprendidos.length === 0) {
+        return [];
+    }
+
+    const idiomaNativo = userData.idioma_nativo_id;
+    const idiomasQuerAprender = idiomasAprendidos.map(row => row.idioma_id);
+
+    // busca usuarios que falam o idiomas desejado ou querem aprender o mesmo idioma
+    const [matches] = await db.query(`
+        SELECT u.id, u.username, u.bio, u.foto_perfil, ui.idioma_id, ui.nivel
+        FROM usuarios u
+        JOIN usuarios_idiomas ui ON u.id = ui.usuario_id
+        WHERE
+            (ui.idioma_id IN (?) AND ui.nivel = 'Avançado' AND u.id != ?)
+            OR
+            (u.idioma_nativo_id IN (?) AND u.id != ?)
+    `, [idiomasQuerAprender, userId, idiomaNativo, userId]);
+
+    return matches;
+}
+
+module.exports = { buscarMatches };
