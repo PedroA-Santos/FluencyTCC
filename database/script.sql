d399a2f1def35ffad17c784a3a7edd4068e9d085
Pedro Antonio <eupedroassuncao@gmail.com>
2025-03-09 15:34:00 -0300

    Trocando script.sql

--- database/script.sql
+++ database/script.sql
@@ -1,88 +1,94 @@
--- Active: 1737138520612@@127.0.0.1@3306@fluencytcc
-drop DATABASE fluencytcc;
+DROP DATABASE IF EXISTS fluencytcc;
 
 CREATE DATABASE fluencytcc
     DEFAULT CHARACTER SET = 'utf8mb4';
 
-use fluencytcc;
+USE fluencytcc;
 
 CREATE TABLE generos  (
-    id int AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     genero VARCHAR(40)
 );
 
-CREATE Table idiomas (
-    id int AUTO_INCREMENT PRIMARY KEY,
-    idioma VARCHAR(50) UNIQUE not NULL
+CREATE TABLE idiomas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    idioma VARCHAR(50) UNIQUE NOT NULL
+);
+
+CREATE TABLE paises (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) UNIQUE NOT NULL
 );
 
 CREATE TABLE usuarios (
-    id int AUTO_INCREMENT PRIMARY key,
-    username VARCHAR(100) not null,
-    email VARCHAR(100) UNIQUE not null,
-    senha VARCHAR(255) not null,
-    idioma_nativo_id int not null,
-    genero_id int not null,
-    bio text DEFAULT '',
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    senha VARCHAR(255) NOT NULL,
+    idioma_nativo_id INT NOT NULL,
+    genero_id INT NOT NULL,
+    data_nascimento DATE NOT NULL,
+    pais_origem_id INT NOT NULL,
+    bio TEXT DEFAULT '',
     foto_perfil VARCHAR(255),
     criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    Foreign Key (idioma_nativo_id) REFERENCES idiomas(id),
-    Foreign Key (genero_id) REFERENCES generos(id)
+    FOREIGN KEY (idioma_nativo_id) REFERENCES idiomas(id),
+    FOREIGN KEY (genero_id) REFERENCES generos(id),
+    FOREIGN KEY (pais_origem_id) REFERENCES paises(id)
 );
 
-
-CREATE Table interesses (
-    id int AUTO_INCREMENT PRIMARY KEY,
-    interesse VARCHAR(50) UNIQUE not null
+CREATE TABLE interesses (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    interesse VARCHAR(50) UNIQUE NOT NULL
 );
 
 CREATE TABLE usuarios_idiomas (
-    id int AUTO_INCREMENT PRIMARY KEY,
-    usuario_id int NOT NULL,
-    idioma_id int not null,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    usuario_id INT NOT NULL,
+    idioma_id INT NOT NULL,
     nivel ENUM ('Básico', 'Intermediário', 'Avançado'),
-    Foreign Key (usuario_id) REFERENCES usuarios(id),
-    Foreign Key (idioma_id) REFERENCES idiomas(id)
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
+    FOREIGN KEY (idioma_id) REFERENCES idiomas(id)
 );
 
 CREATE TABLE usuarios_interesses (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    usuario_id int not null,
-    interesse_id int NOT NULL,
-    Foreign Key (usuario_id) REFERENCES usuarios(id),
-    Foreign Key (interesse_id) REFERENCES interesses(id)
+    usuario_id INT NOT NULL,
+    interesse_id INT NOT NULL,
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
+    FOREIGN KEY (interesse_id) REFERENCES interesses(id)
 );
 
 CREATE TABLE ofensivas (
-    id int AUTO_INCREMENT PRIMARY KEY,
-    usuario_id int not null,
-    idioma_id int NOT NULL,
-    dias_seguidos int DEFAULT 0,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    usuario_id INT NOT NULL,
+    idioma_id INT NOT NULL,
+    dias_seguidos INT DEFAULT 0,
     ultima_atividade TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    Foreign Key (usuario_id) REFERENCES usuarios(id),
-    Foreign Key (idioma_id) REFERENCES idiomas(id)
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
+    FOREIGN KEY (idioma_id) REFERENCES idiomas(id)
 );
 
 CREATE TABLE matches (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    usuario1_id int not null,
-    usuario2_id int not null,
+    usuario1_id INT NOT NULL,
+    usuario2_id INT NOT NULL,
     idioma_comum INT NOT NULL,
     status ENUM('pendente', 'aceito', 'rejeitado'),
     criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    Foreign Key (usuario1_id) REFERENCES usuarios(id),
-    Foreign Key (usuario2_id) REFERENCES usuarios(id),
-    Foreign Key (idioma_comum) REFERENCES idiomas(id)
+    FOREIGN KEY (usuario1_id) REFERENCES usuarios(id),
+    FOREIGN KEY (usuario2_id) REFERENCES usuarios(id),
+    FOREIGN KEY (idioma_comum) REFERENCES idiomas(id)
 );
 
 CREATE TABLE mensagens (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    match_id INT not null,
+    match_id INT NOT NULL,
     remetente_id INT NOT NULL,
-    destinatario_id int NOT NULL,
-    conteudo TEXT not null,
+    destinatario_id INT NOT NULL,
+    conteudo TEXT NOT NULL,
     enviado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    Foreign Key (match_id) REFERENCES matches(id),
-    Foreign Key (remetente_id) REFERENCES usuarios(id),
-    Foreign Key (destinatario_id) REFERENCES usuarios(id)
+    FOREIGN KEY (match_id) REFERENCES matches(id),
+    FOREIGN KEY (remetente_id) REFERENCES usuarios(id),
+    FOREIGN KEY (destinatario_id) REFERENCES usuarios(id)
 );
